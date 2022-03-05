a4f06c4a45d67af42c39f5d78bec58f2c0f9b34c
Huy Do <stevehoang1998@gmail.com>
2022-03-04 18:54:40 -0800

    Fix migration issue

--- db/migrate/20220305014523_add_timezone_to_users.rb
+++ db/migrate/20220305014523_add_timezone_to_users.rb
@@ -1,5 +1,5 @@
 class AddTimezoneToUsers < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :timezome, :string
+    add_column :users, :timezome, :string, default: 'UTC'
   end
 end
