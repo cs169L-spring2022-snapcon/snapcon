07fb586df3d9eddd6c018d820dc70e3462454b3e
myrum <myriampaviles@berkeley.edu>
2022-03-09 12:37:24 -0800

    trying to fix linters

--- db/schema.rb
+++ db/schema.rb
@@ -13,8 +13,8 @@
 ActiveRecord::Schema.define(version: 2021_09_21_221059) do
 
   # These are extensions that must be enabled in order to support this database
-  enable_extension "pg_stat_statements"
-  enable_extension "plpgsql"
+   enable_extension "pg_stat_statements"
+   enable_extension "plpgsql"
 
   create_table "answers", force: :cascade do |t|
     t.string "title"
