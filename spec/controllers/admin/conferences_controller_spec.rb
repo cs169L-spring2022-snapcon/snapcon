f50fe553c592c111654b3765c41741ec923e50e2
myrum <myriampaviles@berkeley.edu>
2022-04-27 20:59:45 -0700

    commeting out timecop line

--- spec/controllers/admin/conferences_controller_spec.rb
+++ spec/controllers/admin/conferences_controller_spec.rb
@@ -3,8 +3,8 @@
 require 'spec_helper'
 
 describe Admin::ConferencesController do
-  t = Time.local(2014, 05, 01, 00, 01, 00)
-  Timecop.travel(t)
+  # t = Time.local(2014, 05, 01, 00, 01, 00)
+  # Timecop.travel(t)
   # It is necessary to use bang version of let to build roles before user
   let!(:organization) { create(:organization, name: 'organization') }
   let!(:conference) { create(:conference, organization: organization, end_date: Date.new(2014, 05, 26) + 15) }
