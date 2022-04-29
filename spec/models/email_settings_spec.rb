c7e4d2d8a236d782048437f4de9abbf614f0d3ba
myrum <myriampaviles@berkeley.edu>
2022-04-29 09:20:07 -0700

    hopefully last time cop fix

--- spec/models/email_settings_spec.rb
+++ spec/models/email_settings_spec.rb
@@ -48,6 +48,13 @@
 require 'spec_helper'
 
 describe EmailSettings do
+  before :each do
+    t = Time.local(2014, 05, 01, 00, 01, 00)
+    Timecop.travel(t)
+  end
+  after(:each) do 
+    Timecop.return 
+  end
   let(:conference) { create(:conference, short_title: 'goto', start_date: Date.new(2014, 05, 01), end_date: Date.new(2014, 05, 06)) }
   let(:user) { create(:user, username: 'johnd', email: 'john@doe.com', name: 'John Doe') }
   let(:event) { create(:event, program: conference.program, title: 'Talk about talks', submitter: user) }
